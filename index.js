ef0ab487ceca597dcd12d92f42d86a8e5b4842d0
Daniel Leite <daniel199257@gmail.com>
2021-03-16 23:54:58 -0300

    alterando

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const cors = require("cors")
+const wokeDyno = require("woke-dyno")
 const app = express()
 const router = require("./src/routes/router.js")
 const { main } = require("./src/auto/schedule")
 
 const port = process.env.PORT || 8080
+
+const uri = "https://bot-ponto.herokuapp.com/"
 
 app.use(express.json())
 app.use(cors())
@@ -13,4 +16,8 @@ app.use(router)
 app.listen(port, async () => {
     console.log(`Connected on port => ${port}`)
     await main()
+    wokeDyno({
+        url: uri,
+        interval: 7200000
+    }).start()
 })
